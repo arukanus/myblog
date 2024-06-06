a6ed1c638c445191b8bccc66cf047bcf7a8e6619
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-06 03:34:32 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,53 +1,16 @@
-import Link from 'next/link';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>{children}</main>
-      <footer>
-        <p>&copy; 2024 Blog Application</p>
-      </footer>
-      <style jsx>{`
-        header, footer {
-          background: #333;
-          color: white;
-          padding: 1rem;
-          text-align: center;
-        }
-        nav ul {
-          display: flex;
-          justify-content: center;
-          list-style: none;
-        }
-        nav ul li {
-          margin: 0 1rem;
-        }
-        main {
-          padding: 1rem;
-        }
-        @media (max-width: 600px) {
-          nav ul {
-            flex-direction: column;
-          }
-        }
-      `}</style>
-    </div>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <main className="container mx-auto p-4">
+      {children}
+    </main>
+    <Footer />
+  </>
+);
 
 export default Layout;
+
