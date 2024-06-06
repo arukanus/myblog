ec60982c0394a1a234a4b240f07d480de55e41e4
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-06 03:35:18 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="text-black p-4 text-center">
+    <p>&copy; 2024 Aruzhan Nussipzhan Production</p>
+  </footer>
+);
+
+export default Footer;
