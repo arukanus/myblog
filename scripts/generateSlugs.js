f68372222aaea244c04938581c75bcfb4766eaf3
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-06 03:48:14 +0100

    Update generateSlugs.js

--- scripts/generateSlugs.js
+++ scripts/generateSlugs.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const slugify = require('slugify');
+
+// Path to the JSON file
+const dataPath = path.join(__dirname, '../public/data/data.json');
+
+// Read the JSON file
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+// Generate slugs and update the JSON data
+const updatedData = data.map(post => ({
+  ...post,
+  slug: slugify(post.title, { lower: true })
+}));
+
+// Write the updated data back to the JSON file
+fs.writeFileSync(dataPath, JSON.stringify(updatedData, null, 2));
+
+console.log('Slugs have been generated and added to data.json');
 
