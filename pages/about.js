d16e3f16c0350ca83562b63da9db754365bb879b
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-06 03:44:45 +0100

    Update and rename blog to about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <h1 className="text-3xl font-bold">About Aruzhan</h1>
+    <h3 className='text-2xl'>Currently homesick</h3>
+  </Layout>
+);
+
+export default About;
