b9e1a2f6ecf4c522f33cd4875845c3b6c9d0264a
arukanus <110685861+arukanus@users.noreply.github.com>
2024-06-06 03:46:29 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Home = () => (
+  <Layout>
+    <h1 className="text-3xl font-bold flex justify-center">Welcome to the Blog</h1>
+    <h3 className='text-2xl'>Find an easter egg on the website!</h3>
+    <h5 className='text-1xl  font-style: italic'>Hint: check the links</h5>
+  </Layout>
+);
+
+export default Home;
